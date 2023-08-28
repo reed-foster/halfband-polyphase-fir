fixed latency calculation bug, works quite well now

optimized_symmetric_zerostuffed_polyphase.py
@@ -11,6 +11,10 @@ class HalfBandFIR:
         self.TAPS[2*N-1] = center_tap
         self.TAPS[2*N::2] = nonzero_first_half[::-1]
 
+
+class FIR0a(HalfBandFIR):
+    def __init__(self):
+        super().__init__(1024, [-3, 477])
 class FIR0(HalfBandFIR):
     def __init__(self):
         super().__init__(2048, [-6, 54, -254, 1230])
@@ -64,7 +68,8 @@ class Shiftreg:
             self.Q = self.state[-1]
 
 class HW_HalfBandFIR:
-    def __init__(self, N_samp_in, FIR):
+    def __init__(self, N_samp_in, FIR, debug=False):
+        self.DEBUG = debug
         self.N_SAMP_IN = N_samp_in
         self.N_SAMP_OUT = N_samp_in // 2
         self.N_TAPS = FIR.N_TAPS
@@ -81,32 +86,48 @@ class HW_HalfBandFIR:
         self.SHIFTREGS_EVEN = [[] for i in range(self.N_SAMP_OUT)]
         even_input_delays = np.zeros((self.N_SAMP_OUT, self.FILTER_DEPTH, 2), dtype=int)
         odd_input_delays = np.zeros(self.N_SAMP_OUT, dtype=int)
-        max_delay = (self.N_TAPS - 2) // self.N_SAMP_IN
+        # just used to assist with simulation/verification
+        self.TOTAL_LATENCY = 0
         for i in reversed(range(self.N_SAMP_OUT)):
-            # process each output, in reverse order
-            odd_input_delays[i] =  -(((-self.N_TAPS)//2 + 1 + 2 * i) // self.N_SAMP_IN) + 2 # extra two stages to match C input with D input of DSP48
+            # C input delay for middle weight
+            odd_input_delays[i] =  -(((-self.N_TAPS)//2 + 1 + 2 * i) // self.N_SAMP_IN) + 3 # extra three stages to match C input with A input of DSP48
+            # additional delay due to cascading DSP48s
+            # note that this can be reduced by using a tree structure (which trades off using more DSP48s for fewer shift register stages)
             stage_delay = 0
+            for j in reversed(range(self.FILTER_DEPTH)):
+                # D input
+                even_input_delays[i,j,0] = -((2 * (i - j)) // self.N_SAMP_IN) + stage_delay + 1 # need one extra delay stage to match D input with A input of DSP48
+                # A input
+                even_input_delays[i,j,1] = -((2 * (i + j + 1) - self.N_TAPS - 1) // self.N_SAMP_IN) + stage_delay 
+                stage_delay += 2
+        m = np.min(even_input_delays)
+        even_input_delays -= m
+        odd_input_delays -= m
+        self.TOTAL_LATENCY = even_input_delays[0,0,0] + 4 + 1
+        for i in range(self.N_SAMP_OUT):
             self.SHIFTREGS_ODD[i] = Shiftreg(odd_input_delays[i])
             self.SHIFTREGS_EVEN[i] = [None for j in range(2*self.FILTER_DEPTH)]
-            for j in reversed(range(self.FILTER_DEPTH)):
-                even_input_delays[i,j,0] = -((2 * (i - j)) // self.N_SAMP_IN) + stage_delay
-                even_input_delays[i,j,1] = -((2 * (i + j + 1) - self.N_TAPS - 1) // self.N_SAMP_IN) + stage_delay - 1 # need one less delay stage to match A input with D input of DSP48
+            for j in range(self.FILTER_DEPTH):
+                #d_max = even_input_delays[i,j,0] + 5 + 2*j - 1
+                #if d_max > self.TOTAL_LATENCY:
+                #    self.TOTAL_LATENCY = d_max
                 self.SHIFTREGS_EVEN[i][2*j] = Shiftreg(even_input_delays[i,j,0]) # D input
                 self.SHIFTREGS_EVEN[i][2*j+1] = Shiftreg(even_input_delays[i,j,1]) # A input
-                # account for delay from cascading multiple DSP48s:
-                # note that this can be reduced by using a tree structure (which trades off using more DSP48s for fewer shift register stages)
-                stage_delay += 2
-                print(f'(i,j) = {i,j}, (x1,x2) = {((2 * (i - j)) % self.N_SAMP_IN, (2 * (i + j + 1) - self.N_TAPS - 1) % self.N_SAMP_IN)}')
-        print(even_input_delays)
-        print(odd_input_delays)
+        if self.DEBUG:
+            print(f'N_TAPS = {self.N_TAPS}, FILTER_DEPTH = {self.FILTER_DEPTH}, TOTAL_LATENCY = {self.TOTAL_LATENCY}')
+            print(f'even_input_delays = {even_input_delays}')
+            print(f'odd_input_delays = {odd_input_delays}')
+            for i in range(self.N_SAMP_OUT):
+                for j in range(self.FILTER_DEPTH):
+                    print(f'(i,j) = {i,j}, (xD,xA,xC) = {(2 * (i - j)) % self.N_SAMP_IN, (2 * (i + j + 1) - self.N_TAPS - 1) % self.N_SAMP_IN, ((-self.N_TAPS)//2 + 1 + 2 * i) % self.N_SAMP_IN}')
 
     def step(self, x_in):
         for i in range(self.N_SAMP_OUT):
             self.Y[i] = self.DSP48s[i][0].P
             for j in range(self.FILTER_DEPTH):
                 # DSP48.step(A, D, C)
-                x1 = x_in[(2 * (i - j)) % self.N_SAMP_IN]
-                x2 = x_in[(2 * (i + j + 1) - self.N_TAPS - 1) % self.N_SAMP_IN]
+                x1 = x_in[(2 * (i - j)) % self.N_SAMP_IN] # D input
+                x2 = x_in[(2 * (i + j + 1) - self.N_TAPS - 1) % self.N_SAMP_IN] # A input
                 D = x1 if self.SHIFTREGS_EVEN[i][2*j].BYPASS else self.SHIFTREGS_EVEN[i][2*j].Q
                 A = x2 if self.SHIFTREGS_EVEN[i][2*j+1].BYPASS else self.SHIFTREGS_EVEN[i][2*j+1].Q
                 C = self.DSP48s[i][j+1].P if j < self.FILTER_DEPTH - 1 else self.SHIFTREGS_ODD[i].Q
@@ -126,64 +147,48 @@ if __name__ == '__main__':
     #x = 0.2*np.random.randn(N_in)
     x = np.sin(2*np.pi*5e6*t_in) + np.sin(2*np.pi*fs_in/3*t_in)
 
-    N_SAMP_ADC = 8 # samples per cycle from ADC
+    for k in range(3):
+        if k == 0:
+            N_SAMP_ADC = 4 # samples per cycle from ADC
+        elif k == 1:
+            N_SAMP_ADC = 8 # samples per cycle from ADC
+        else:
+            N_SAMP_ADC = 16 # samples per cycle from ADC
 
-    # initialize FIR taps
-    fir0 = FIR0()
-    fir1 = FIR1()
-    fir2 = FIR2()
+        # initialize FIR taps
+        firs = [FIR0a(), FIR0(), FIR1(), FIR2()]
+        debug = [False, False, False, False]
+        plot = [True, True, True, True]
 
-    print(fir0.N_TAPS)
+        # apply filter in software to signal
+        sw_sim_result = [lfilter(fir.TAPS, 1.0, x)[::2] for fir in firs]
+        sw_poly_sim_result = [lfilter(fir.TAPS[0::2], 1.0, x[0::2]) + lfilter(fir.TAPS[1::2] + [0], 1.0, np.concatenate(([0], x[1:-2:2]))) for fir in firs]
 
-    hw_sim_fir0 = HW_HalfBandFIR(N_SAMP_ADC, FIR0())
-    #hw_sim_fir1 = HW_HalfBandFIR(N_SAMP_ADC, FIR1())
-    #hw_sim_fir2 = HW_HalfBandFIR(N_SAMP_ADC, FIR2())
+        # simulate hardware for filter
+        hw_sim_firs = [HW_HalfBandFIR(N_SAMP_ADC, fir, dbg) for fir, dbg in zip(firs,debug)]
+        hw_sim_result = np.zeros((len(firs),N_in//2))
+        for cycle in range(N_in//N_SAMP_ADC):
+            for i in range(len(firs)):
+                if cycle >= hw_sim_firs[i].TOTAL_LATENCY:
+                    cycle0 = cycle - hw_sim_firs[i].TOTAL_LATENCY
+                    hw_sim_result[i,(N_SAMP_ADC//2)*cycle0:(N_SAMP_ADC//2)*(cycle0+1)] = hw_sim_firs[i].Y
+                hw_sim_firs[i].step(x[N_SAMP_ADC*cycle:N_SAMP_ADC*(cycle+1)])
 
-    fir0_hw = np.zeros(N_in//2)
-    #fir1_hw = np.zeros(N_in//2)
-    #fir2_hw = np.zeros(N_in//2)
-
-    for i in range(N_in//N_SAMP_ADC):
-        fir0_hw[(N_SAMP_ADC//2)*i:(N_SAMP_ADC//2)*(i+1)] = hw_sim_fir0.Y
-        hw_sim_fir0.step(x[N_SAMP_ADC*i:N_SAMP_ADC*(i+1)])
-        #fir1_hw[(N_SAMP_ADC//2)*i:(N_SAMP_ADC//2)*(i+1)] = hw_sim_fir1.Y
-        #hw_sim_fir1.step(x[N_SAMP_ADC*i:N_SAMP_ADC*(i+1)])
-        #fir2_hw[(N_SAMP_ADC//2)*i:(N_SAMP_ADC//2)*(i+1)] = hw_sim_fir2.Y
-        #hw_sim_fir2.step(x[N_SAMP_ADC*i:N_SAMP_ADC*(i+1)])
-
-    #exit()
-
-    # apply filter in software to signal
-    fir0_sw = lfilter(fir0.TAPS, 1.0, x)[::2]
-    fir1_sw = lfilter(fir1.TAPS, 1.0, x)[::2]
-    fir2_sw = lfilter(fir2.TAPS, 1.0, x)[::2]
-    fir0_sw_poly = lfilter(fir0.TAPS[0::2], 1.0, x[0::2]) + lfilter(fir0.TAPS[1::2] + [0], 1.0, np.concatenate(([0], x[1:-2:2])))
-    fir1_sw_poly = lfilter(fir1.TAPS[0::2], 1.0, x[0::2]) + lfilter(fir1.TAPS[1::2] + [0], 1.0, np.concatenate(([0], x[1:-2:2])))
-    fir2_sw_poly = lfilter(fir2.TAPS[0::2], 1.0, x[0::2]) + lfilter(fir2.TAPS[1::2] + [0], 1.0, np.concatenate(([0], x[1:-2:2])))
-
-    # simulate hardware for filter
-
-    # plot results
-    fig, ax = plt.subplots(3,1,sharex=True)
-    #fig, ax = plt.subplots(4,4)
-    ax[0].plot(t_in, x, label='input')
-    ax[1].plot(t_in[::2], fir0_sw_poly, label='fir0 software (polyphase) impl')
-    ax[2].plot(t_in[::2], fir0_hw, label='fir0 hardware impl')
-    #ax[0,3].plot(t_in[::2], fir0_sw - fir0_sw_poly, label='error')
-
-    #ax[1,0].plot(t_in[::2], fir0_sw, label='fir0 software impl')
-    #ax[1,1].plot(t_in[::2], fir0_sw_poly, label='fir0 software (polyphase) impl')
-    #ax[1,2].plot(t_in[::2], fir0_hw, label='fir0 hardware impl')
-    #ax[1,3].plot(t_in[::2], fir0_sw - fir0_sw_poly, label='error')
-
-    #ax[2,0].plot(t_in[::2], fir1_sw, label='fir1 software impl')
-    #ax[2,1].plot(t_in[::2], fir1_sw_poly, label='fir1 software (polyphase) impl')
-    #ax[2,2].plot(t_in[::2], fir1_hw, label='fir1 hardware impl')
-    #ax[2,3].plot(t_in[::2], fir1_sw - fir1_sw_poly, label='error')
-
-    #ax[3,0].plot(t_in[::2], fir2_sw, label='fir2 software impl')
-    #ax[3,1].plot(t_in[::2], fir2_sw_poly, label='fir2 software (polyphase) impl')
-    #ax[3,2].plot(t_in[::2], fir2_hw, label='fir2 hardware impl')
-    #ax[3,3].plot(t_in[::2], fir2_sw - fir2_sw_poly, label='error')
+        # plot results
+        for i in range(len(firs)):
+            if not plot[i]:
+                continue
+            fig, ax = plt.subplots(3,1,sharex=True)
+            ax[0].plot(t_in, x, label='input')
+            ax[1].plot(t_in[::2], sw_sim_result[i], label=f'fir{i} software impl')
+            ax[1].plot(t_in[::2], sw_poly_sim_result[i], label=f'fir{i} software (polyphase) impl')
+            ax[1].plot(t_in[::2], hw_sim_result[i], label=f'fir{i} hardware impl')
+            t_in_trim = t_in[:-hw_sim_firs[i].TOTAL_LATENCY*N_SAMP_ADC:2]
+            fir_sw_poly_trim = sw_poly_sim_result[i][:-hw_sim_firs[i].TOTAL_LATENCY*(N_SAMP_ADC//2)]
+            fir_hw_trim = hw_sim_result[i,:-hw_sim_firs[i].TOTAL_LATENCY*(N_SAMP_ADC//2)]
+            ax[2].plot(t_in_trim, fir_sw_poly_trim - fir_hw_trim, label='error')
+            for j in range(len(ax)):
+                ax[j].legend()
+            fig.suptitle(f'firidx = {i}, N_TAPS = {firs[i].N_TAPS}, N_SAMP_ADC = {N_SAMP_ADC}')
 
     plt.show()